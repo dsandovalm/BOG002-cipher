c5aa94ecbc888d33d8ddffef2270dc5c284b1e79
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-02-22 11:57:57 -0500

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,19 @@
 const cipher = {
   encode: function(offset,string){
-    let alphabet = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     let mensaje = '';
     //Convierte el string en un array
     let array = string.split('');
     //Toma cada elemento del array y verifica que esté en el alfabeto
     for(let i=0;i<array.length;i++){
       let index = alphabet.indexOf(array[i]);
+      
       if(index == -1){
         //Si no está, no se hace nada
-        mensaje =+ array[i];
+        mensaje = mensaje + array[i];
       }else{
         //Si está se encripta
-        mensaje =+ alphabet[(index+offset)%alphabet.lenght];
+        mensaje = mensaje + alphabet[(index+offset)%alphabet.length];
       }
     }
     return mensaje
