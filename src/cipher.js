f56f85c38e8987903977dc988b1e3dbf3c6ab530
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-02-24 18:35:29 -0500

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,57 @@
 const cipher = {
   encode: function(offset,string){
-    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    offset = offset % alphabet.length;
+    const alphabet = 26;
+    offset =  (offset === null) ? undefined : offset % alphabet;
+    string = typeof string === 'string' ? string : undefined;
     let mensaje = '';
-    //Toma cada elemento del array y verifica que esté en el alfabeto
+
+    //Toma cada elemento del string y verifica que su codigo ASCII sea de una letra
+
     for(let i=0;i<string.length;i++){
-      let index = alphabet.indexOf(string[i]);
-      
-      if(index == -1){
-        //Si no está, no se hace nada
-        mensaje = mensaje + string[i];
-      }else{
-        //Si está se encripta
-        mensaje = mensaje + alphabet[(index+offset)%alphabet.length];
+      let charCode = string.charCodeAt(i);
+      let tipo = (charCode >= 65 && charCode < 91) ? 'Mayus' : (charCode >= 97 && charCode < 123) ? 'Minus' : 'Otro'; 
+      switch (tipo){
+        case 'Mayus':
+          //Mayusculas
+          mensaje = mensaje + String.fromCharCode(65 + (charCode + offset - 65) % alphabet);
+          break
+        case 'Minus':
+          //Minusculas
+          mensaje = mensaje + String.fromCharCode(97 + (charCode + offset - 97) % alphabet);
+          break
+        default:
+          //Caracteres que no se encuentran en el alfabeto
+          mensaje = mensaje + string[i];
+          break
       }
     }
     return mensaje
   },
+
   decode: function(offset,string){
-    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    offset = offset % alphabet.length;
+    const alphabet = 26;
+    offset =  (offset === null) ? undefined : offset % alphabet;
+    string = typeof string === 'string' ? string : undefined;
     let mensaje = '';
     for(let i=0;i<string.length;i++){
-      let index = alphabet.indexOf(string[i]);
-      
-      if(index == -1){
-        mensaje = mensaje + string[i];
-      }else{
-        mensaje = mensaje + alphabet[(alphabet.length+index-offset)%alphabet.length];
+      let charCode = string.charCodeAt(i);
+      let tipo = (charCode >= 65 && charCode < 91) ? 'Mayus' : (charCode >= 97 && charCode < 123) ? 'Minus' : 'Otro'; 
+      switch (tipo){
+        case 'Mayus':
+          mensaje = mensaje + String.fromCharCode(65 + (alphabet + charCode - offset - 65) % alphabet);
+          break
+        case 'Minus':
+          mensaje = mensaje + String.fromCharCode(97 + (alphabet + charCode - offset - 97) % alphabet);
+          break
+        default:
+          mensaje = mensaje + string[i];
+          break
       }
     }
     return mensaje
   },
-  // ...
 };
    
 
 export default cipher;
+
