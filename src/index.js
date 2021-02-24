fd0bb30afbd9137e913eb412644aff784c18a6e9
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-02-24 18:47:53 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import cipher from './cipher.js';
 
 function encode(){
-    let mensaje = document.getElementById('texto').value;
+    let mensaje = document.getElementById('encodeT').value;
     let offset = document.getElementById('offset').value;
-    document.getElementById('texto').value = cipher.encode(offset, mensaje);
+    document.getElementById('decodeT').value = cipher.encode(offset, mensaje);
 } 
 
 function decode(){
-    let mensaje = document.getElementById('texto').value;
+    let mensaje = document.getElementById('decodeT').value;
     let offset = document.getElementById('offset').value;
-    document.getElementById('texto').value = cipher.decode(offset, mensaje);
+    document.getElementById('encodeT').value = cipher.decode(offset, mensaje);
 } 
 
-document.getElementById('buttonEncode').addEventListener('click', encode);
-document.getElementById('buttonDecode').addEventListener('click', decode);
+document.getElementById('encodeB').addEventListener('click', encode);
+document.getElementById('decodeB').addEventListener('click', decode);
 
