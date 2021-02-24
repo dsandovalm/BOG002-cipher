98992fc53facd0ada630e59943ae17dbbd048d64
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-02-24 18:39:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-console.log(cipher.encode('HOLA MUNDO ¿Cómo estás?'))
+function encode(){
+    let mensaje = document.getElementById('texto').value;
+    let offset = document.getElementById('offset').value;
+    document.getElementById('texto').value = cipher.encode(offset, mensaje);
+} 
+
+function decode(){
+    let mensaje = document.getElementById('texto').value;
+    let offset = document.getElementById('offset').value;
+    document.getElementById('texto').value = cipher.decode(offset, mensaje);
+} 
+
+document.getElementById('buttonEncode').addEventListener('click', encode);
+document.getElementById('buttonDecode').addEventListener('click', decode);
+
