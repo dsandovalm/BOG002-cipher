adac348313eebf941e2f4053c1388a2ec6602f12
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-03-03 13:46:17 -0500

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,28 @@
-const genString = (a,base,off) => ( String.fromCharCode(a + (26 + base + off - a) % 26));
+const genString = (a, base, off) => (String.fromCharCode(a + (26 + base + off - a) % 26));
 
 const cipher = {
-  encode: function(offset,string){
-    offset =  isNaN(parseInt(offset)) ? undefined : ((offset % 26) + 26) % 26;
-    string =  (typeof string === 'string') ? string : undefined;
+  encode: function (offset, string) {
+    offset = isNaN(parseInt(offset)) ? undefined : ((offset % 26) + 26) % 26;
+    string = (typeof string === 'string') ? string : undefined;
     let mensaje = '';
 
-    if(offset == undefined){
+    if (offset == undefined) {
       alert('Revise que el offset sea un número');
-    } else {
-      //Toma cada elemento del string y verifica que su codigo ASCII sea de una letra
+    }
+    //Toma cada elemento del string y verifica que su codigo ASCII sea de una letra
 
-    for(let i=0;i<string.length;i++){
+    for (let i = 0; i < string.length; i++) {
       let charCode = string.charCodeAt(i);
-      let tipo = (charCode >= 65 && charCode < 91) ? 'Mayus' : (charCode >= 97 && charCode < 123) ? 'Minus' : 'Otro'; 
-      
-      switch (tipo){
+      let tipo = (charCode >= 65 && charCode < 91) ? 'Mayus' : (charCode >= 97 && charCode < 123) ? 'Minus' : 'Otro';
+
+      switch (tipo) {
         case 'Mayus':
           //Mayusculas
-          mensaje = mensaje + genString(65,charCode,offset);
+          mensaje = mensaje + genString(65, charCode, offset);
           break
         case 'Minus':
           //Minusculas
-          mensaje = mensaje + genString(97,charCode,offset);
+          mensaje = mensaje + genString(97, charCode, offset);
           break
         default:
           //Caracteres que no se encuentran en el alfabeto
@@ -30,36 +30,34 @@ const cipher = {
           break
       }
     }
-    }
     return mensaje
-    },
-    
-  decode: function(offset,string){
-    offset =  isNaN(parseInt(offset)) ? undefined : ((offset % 26) + 26) % 26;
-    string =  (typeof string === 'string') ? string : undefined;
-    let mensaje = '';
-    
-    //Toma cada elemento del string y verifica que su codigo ASCII sea de una letra
-    if(offset == undefined){
-      alert('Revise que el offset sea un número');
-    } else {
+  },
 
-    for(let i=0;i<string.length;i++){
+  decode: function (offset, string) {
+    offset = isNaN(parseInt(offset)) ? undefined : ((offset % 26) + 26) % 26;
+    string = (typeof string === 'string') ? string : undefined;
+    let mensaje = '';
+
+    //Toma cada elemento del string y verifica que su codigo ASCII sea de una letra
+    if (offset == undefined) {
+      alert('Revise que el offset sea un número');
+    }
+
+    for (let i = 0; i < string.length; i++) {
       let charCode = string.charCodeAt(i);
-      let tipo = (charCode >= 65 && charCode < 91) ? 'Mayus' : (charCode >= 97 && charCode < 123) ? 'Minus' : 'Otro'; 
-      switch (tipo){
+      let tipo = (charCode >= 65 && charCode < 91) ? 'Mayus' : (charCode >= 97 && charCode < 123) ? 'Minus' : 'Otro';
+      switch (tipo) {
         case 'Mayus':
-          mensaje = mensaje + genString(65,charCode,-offset);
+          mensaje = mensaje + genString(65, charCode, -offset);
           break
         case 'Minus':
-          mensaje = mensaje + genString(97,charCode,-offset);
+          mensaje = mensaje + genString(97, charCode, -offset);
           break
         default:
           mensaje = mensaje + string[i];
           break
       }
     }
-  }
     return mensaje
   },
 };
