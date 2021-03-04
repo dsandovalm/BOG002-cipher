3d5c9f866049cbc4347566e2e7f4d3a2bf4ed051
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-03-04 10:04:36 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,16 +8,20 @@ document.getElementById("encodeB").addEventListener("click", buttonEncode);
 
 function buttonEncode() {
   let offset = document.getElementById('offset').value;
-  let texto = document.getElementById('encodeT').value;
+  let texto = document.getElementById('encodeT');
   let resultado = document.getElementById('decodeT');
-  resultado.innerHTML = cipher.encode(offset,texto);
+  texto.style.color = '#6F7085';
+  resultado.style.color = '#000'; 
+  resultado.value = cipher.encode(offset,texto.value);
 }
 
 document.getElementById("decodeB").addEventListener("click", buttonDecode);
 
 function buttonDecode() {
   let offset = document.getElementById('offset').value;
-  let texto = document.getElementById('decodeT').value;
+  let texto = document.getElementById('decodeT');
   let resultado = document.getElementById('encodeT');
-  resultado.value = cipher.decode(offset,texto);
+  texto.style.color = '#6F7085';
+  resultado.style.color = '#000'; 
+  resultado.value = cipher.decode(offset,texto.value);
 }
